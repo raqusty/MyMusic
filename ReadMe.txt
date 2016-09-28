230fb8c94f8bb5120ada1993ee18c51433e1e690
raqusty <583006809@qq.com>
2016-09-28 17:54:06 +0800

    2016/9/28

---
+++ ReadMe.txt
@@ -0,0 +1,26 @@
+搭建框架
+
+--  创建一个最新项目，用24sdk构建
+    gradle:2.2.0 ,  2.14.1
+
+一： 划分好各个模块，分好各个包: 1、common, 2、module 3、util  4、widget
+
+    1: common
+        --  引入之前做好的基础activity ，Fragment
+
+    2: module
+    3: util
+    4: widget
+
+
+
+二：配置文件
+    1：  bulid.gradle
+         --  添加 multidex ，解决65K方法
+         --  引入ButterKnife
+
+    2:   manifest
+
+
+
+
--- app/src/main/java/music/hayasi/android/com/mymusic/common/activity/BaseActivity.java
+++ app/src/main/java/music/hayasi/android/com/mymusic/common/activity/BaseActivity.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
+import music.hayasi.android.com.mymusic.R;
+import music.hayasi.android.com.mymusic.common.widget.MultiStateView;
 
 public abstract class BaseActivity extends AppCompatActivity implements IToolBar,
         IBasePageOperations {
@@ -95,100 +101,100 @@ public abstract class BaseActivity extends AppCompatActivity implements IToolBar
     }
 
 
-//    /**
-//     * 显示Loading页面
-//     */
-//    protected void showLoadingView(MultiStateView multiStateView) {
-//        if (multiStateView == null) {
-//            return;
-//        }
-//
-//        multiStateView.setViewState(MultiStateView.ViewState.LOADING);
-//    }
-//
-//    /**
-//     * 显示默认的无数据的提示页面
-//     */
-//    protected void showEmptyView(MultiStateView multiStateView) {
-//        showEmptyView(multiStateView,null, null);
-//    }
-//
-//    /**
-//     * 显示无数据的提示页面
-//     *
-//     * @param emptyPhotoResId 空数据图片资源Id(可以传null)
-//     * @param emptyTips       空数据文字提示(可以传null)
-//     */
-//    protected void showEmptyView(MultiStateView multiStateView, Integer emptyPhotoResId, String emptyTips) {
-//        if (multiStateView == null) {
-//            return;
-//        }
-//
-//        if (emptyPhotoResId != null) {
-//            ((ImageView) multiStateView
-//                    .getView(MultiStateView.ViewState.EMPTY).findViewById(
-//                            R.id.empty_view_iv_empty_photo))
-//                    .setImageResource(emptyPhotoResId);
-//        }
-//        if (emptyTips != null) {
-//            ((TextView) multiStateView.getView(MultiStateView.ViewState.EMPTY)
-//                    .findViewById(R.id.empty_view_tv_text_tips))
-//                    .setText(emptyTips);
-//        }
-//        multiStateView.setViewState(MultiStateView.ViewState.EMPTY);
-//    }
-//
-//    /**
-//     * 显示网络异常的提示页面
-//     */
-//    protected void showNetworkErrorView(MultiStateView multiStateView) {
-//        showNetworkErrorView(multiStateView,null);
-//    }
-//
-//    /**
-//     * 显示网络异常的提示页面
-//     *
-//     * @param errorTips 异常数据文字提示(可以传null)
-//     */
-//    protected void showNetworkErrorView(MultiStateView multiStateView,String errorTips) {
-//        if (multiStateView == null) {
-//            return;
-//        }
-//
-//        if (!TextUtils.isEmpty(errorTips)) {
-//            ((TextView) multiStateView.getView(MultiStateView.ViewState.ERROR)
-//                    .findViewById(R.id.error_view_tv_text_tips))
-//                    .setText(errorTips);
-//        }
-//
-//        multiStateView.setViewState(MultiStateView.ViewState.ERROR);
-//        multiStateView.getView(MultiStateView.ViewState.ERROR).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                refreshPage();
-//            }
-//        });
-//    }
-//
-//    /**
-//     * 显示正常内容的页面
-//     */
-//    protected void showContentView(MultiStateView multiStateView) {
-//        if (multiStateView == null) {
-//            return;
-//        }
-//
-//        multiStateView.setViewState(MultiStateView.ViewState.CONTENT);
-//    }
-//
-//    /**
-//     * 获取当前页面的状态
-//     *
-//     * @return 当前页面状态
-//     */
-//    protected MultiStateView.ViewState getPageState(MultiStateView multiStateView) {
-//        return multiStateView == null ? null : multiStateView.getViewState();
-//    }
+    /**
+     * 显示Loading页面
+     */
+    protected void showLoadingView(MultiStateView multiStateView) {
+        if (multiStateView == null) {
+            return;
+        }
+
+        multiStateView.setViewState(MultiStateView.ViewState.LOADING);
+    }
+
+    /**
+     * 显示默认的无数据的提示页面
+     */
+    protected void showEmptyView(MultiStateView multiStateView) {
+        showEmptyView(multiStateView,null, null);
+    }
+
+    /**
+     * 显示无数据的提示页面
+     *
+     * @param emptyPhotoResId 空数据图片资源Id(可以传null)
+     * @param emptyTips       空数据文字提示(可以传null)
+     */
+    protected void showEmptyView(MultiStateView multiStateView, Integer emptyPhotoResId, String emptyTips) {
+        if (multiStateView == null) {
+            return;
+        }
+
+        if (emptyPhotoResId != null) {
+            ((ImageView) multiStateView
+                    .getView(MultiStateView.ViewState.EMPTY).findViewById(
+                            R.id.empty_view_iv_empty_photo))
+                    .setImageResource(emptyPhotoResId);
+        }
+        if (emptyTips != null) {
+            ((TextView) multiStateView.getView(MultiStateView.ViewState.EMPTY)
+                    .findViewById(R.id.empty_view_tv_text_tips))
+                    .setText(emptyTips);
+        }
+        multiStateView.setViewState(MultiStateView.ViewState.EMPTY);
+    }
+
+    /**
+     * 显示网络异常的提示页面
+     */
+    protected void showNetworkErrorView(MultiStateView multiStateView) {
+        showNetworkErrorView(multiStateView,null);
+    }
+
+    /**
+     * 显示网络异常的提示页面
+     *
+     * @param errorTips 异常数据文字提示(可以传null)
+     */
+    protected void showNetworkErrorView(MultiStateView multiStateView,String errorTips) {
+        if (multiStateView == null) {
+            return;
+        }
+
+        if (!TextUtils.isEmpty(errorTips)) {
+            ((TextView) multiStateView.getView(MultiStateView.ViewState.ERROR)
+                    .findViewById(R.id.error_view_tv_text_tips))
+                    .setText(errorTips);
+        }
+
+        multiStateView.setViewState(MultiStateView.ViewState.ERROR);
+        multiStateView.getView(MultiStateView.ViewState.ERROR).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refreshPage();
+            }
+        });
+    }
+
+    /**
+     * 显示正常内容的页面
+     */
+    protected void showContentView(MultiStateView multiStateView) {
+        if (multiStateView == null) {
+            return;
+        }
+
+        multiStateView.setViewState(MultiStateView.ViewState.CONTENT);
+    }
+
+    /**
+     * 获取当前页面的状态
+     *
+     * @return 当前页面状态
+     */
+    protected MultiStateView.ViewState getPageState(MultiStateView multiStateView) {
+        return multiStateView == null ? null : multiStateView.getViewState();
+    }
 
     @Override
     public void refreshPage() {
