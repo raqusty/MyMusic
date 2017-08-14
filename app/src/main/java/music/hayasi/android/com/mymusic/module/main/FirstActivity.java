1f5b3ee1d7fe12b027adecf7912c8c8a8020494e
linzehao <linzehao@aipai.com>
2017-08-14 09:40:20 +0800

    11

--- app/src/main/java/music/hayasi/android/com/mymusic/module/main/FirstActivity.java
+++ app/src/main/java/music/hayasi/android/com/mymusic/module/main/FirstActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -66,10 +66,7 @@ public class FirstActivity extends BaseActivity {
     @Override
     public void initViews() {
         initData();
-//        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(mContext);
-        mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         mRecyclerView.setAdapter(mAdapter = new HomeAdapter());
 
         mAdapter.setOnItemClickLitener(new OnItemClickLitener() {
